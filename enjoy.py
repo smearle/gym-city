feeff712008b19a4bdcfeb67c8f94b0cfccc548c
Sam <smearle93@gmail.com>
2019-08-11 21:33:37 -0400

    Cleaning up

--- enjoy.py
+++ enjoy.py
@@ -35,6 +35,7 @@ checkpoint = torch.load(os.path.join(args.load_dir, env_name + '.tar'))
 saved_args = checkpoint['args']
 env_name = saved_args.env_name
 
+args.power_puzzle = saved_args.power_puzzle
 env = make_vec_envs(env_name, args.seed + 1000, 1,
                             None, None, args.add_timestep, device='cpu',
                             allow_early_resets=False,
