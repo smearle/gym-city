30b4a3d44d29bff26355d1ea09111fba3eba5635
Sam Earle` <smearle93@gmail.com>
2019-12-29 14:17:37 -0500

    name change to gym-city

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 install:
-	cd gym_micropolis/envs/micropolis/MicropolisCore; make; sudo make install
+	cd gym_city/envs/micropolis/MicropolisCore; make; sudo make install
 
 teach_GoL:
 	python3 main_teacher.py --env-name GoLMultiEnv-v0 --algo a2c --experiment test --map-width 16 --model FullyConv --num-frames 100000000 --eval-interval 1000 --num-processes 500 --render
--- gym_city/__init__.py
+++ gym_city/__init__.py
@@ -2,20 +2,20 @@ from gym.envs.registration import register
 
 register(
     id='MicropolisEnv-v0',
-    entry_point='gym_micropolis.envs:MicropolisEnv',
+    entry_point='gym_city.envs:MicropolisEnv',
     kwargs={'MAP_X':14, 'MAP_Y':14}
 )
 register(
     id='MicropolisPaintEnv-v0',
-    entry_point='gym_micropolis.envs:MicropolisPaintEnv',
+    entry_point='gym_city.envs:MicropolisPaintEnv',
     kwargs={'MAP_X':14, 'MAP_Y':14}
 )
 register(
     id='MicropolisWalkEnv-v0',
-    entry_point='gym_micropolis.envs:MicroWalkEnv',
+    entry_point='gym_city.envs:MicroWalkEnv',
 )
 
 register(
     id='MicropolisArcadeEnv-v0',
-    entry_point='gym_micropolis.envs:MicroArcadeEnv',
+    entry_point='gym_city.envs:MicroArcadeEnv',
 )
